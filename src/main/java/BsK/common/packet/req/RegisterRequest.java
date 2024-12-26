96e5b8ec979c9b153420f0f507659639215399df
lethanhdat <91755733+ldss13@users.noreply.github.com>
2024-12-25 22:17:15 -0600

    update a bit because of holiday

--- src/main/java/BsK/common/packet/req/RegisterRequest.java
+++ src/main/java/BsK/common/packet/req/RegisterRequest.java
@@ -4,6 +4,8 @@ import BsK.common.packet.Packet;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Data
 public class RegisterRequest implements Packet {
@@ -11,4 +13,14 @@ public class RegisterRequest implements Packet {
   String username;
   String password;
   String name;
+  String role;
+  LocalDateTime timestamp;
+
+  public RegisterRequest(String username, String password, String name, String role) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.role = role;
+    this.timestamp = LocalDateTime.now(); // Automatically sets the current time
+  }
 }
